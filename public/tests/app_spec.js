eae72c99e1df0c30f944f9d58baf54b48677b72a
Makoto Yonezawa <vestige_sync@me.com>
2019-11-30 14:24:43 +0900

    add test

---
+++ public/tests/app_spec.js
@@ -0,0 +1,6 @@
+describe('Learn Js', function() {
+    it ('can show a problem view', function() {
+        learnjs.showView('#problem-1');
+        expect($('.view-container .problem-view').length).toEqual(1);
+    });
+});
