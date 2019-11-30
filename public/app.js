25a026576fa7695acec7f718bdef6790579c0900
Makoto Yonezawa <vestige_sync@me.com>
2019-11-30 15:16:59 +0900

    modify first test add showView func

---
+++ public/app.js
@@ -0,0 +1,7 @@
+'use strict';
+var learnjs = {};
+
+learnjs.showView = function(hash) {
+    var problemView = $('<div class="problem-view">').text('Comming soon.');
+    $('.view-container').empty().append(problemView);
+}
