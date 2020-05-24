7923719faecb924ffb24aedb9485e763a571dba5
Makoto Yonezawa <vestige_sync@me.com>
2020-05-24 15:13:33 +0900

    3300 test add user answer action

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,40 +1,64 @@
-describe('LearnJS', function(){
-    it('can show a problem view', function(){
-        learnjs.showView('#problem-1');
-        expect($('.view-container .problem-view').length).toEqual(1);
+describe('LearnJS', function() {
+    it('can show a problem view', function() {
+      learnjs.showView('#problem-1');
+      expect($('.view-container .problem-view').length).toEqual(1);
     });
-    it('show the landing page view with no hash', function() {
-        learnjs.showView('');
-        expect($('.view-container .landing-view').length).toEqual(1);
+  
+    it('shows the landing page view when there is no hash', function() {
+      learnjs.showView('');
+      expect($('.view-container .landing-view').length).toEqual(1);
     });
-    it('check the view parameters', function(){
-        spyOn(learnjs, 'problemView');
-        learnjs.showView('#problem-42');
-        expect(learnjs.problemView).toHaveBeenCalledWith('42');
+  
+    it('passes the hash view parameter to the view function', function() {
+      spyOn(learnjs, 'problemView');
+      learnjs.showView('#problem-42');
+      expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
-    describe('problem view', function(){
-        it('has a title number includes the problem number', function(){
-            var view = learnjs.problemView('1');
-            expect(view.find('.title').text()).toEqual('Problem #1');
+  
+    it('invokes the router when loaded', function() {
+      spyOn(learnjs, 'showView');
+      learnjs.appOnReady();
+      expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+  
+    it('subscribes to the hash change event', function() {
+      learnjs.appOnReady();
+      spyOn(learnjs, 'showView');
+      $(window).trigger('hashchange');
+      expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+  
+    describe('problem view', function() {
+      var view;
+      beforeEach(function() {
+        view = learnjs.problemView('1');
+      });
+  
+      it('has a title that includes the problem number', function() {
+        expect(view.find('.title').text()).toEqual('Problem #1');
+      });
+  
+      it('shows the description', function() {
+        expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+      });
+  
+      it('shows the problem code', function() {
+        expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
+      });
+  
+      describe('answer section', function() {
+        it('can check a correct answer by hitting a button', function() {
+          view.find('.answer').val('true');
+          view.find('.check-btn').click();
+          expect(view.find('.result').text()).toEqual('Correct!');
         });
-        it('show the description', function() {
-            var view = learnjs.problemView('1');
-            expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');            
+  
+        it('rejects an incorrect answer', function() {
+          view.find('.answer').val('false');
+          view.find('.check-btn').click();
+          expect(view.find('.result').text()).toEqual('Incorrect!');
         });
-        it('show the probelm code', function() {
-            var view = learnjs.problemView('1');
-            expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');            
-        });
+      });
     });
-    it('invokes the router when loaded', function(){
-        spyOn(learnjs, 'showView');
-        learnjs.appOnReady();
-        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
-    });
-    it('subscribes to the hash change event', function(){
-        learnjs.appOnReady();
-        spyOn(learnjs, 'showView');
-        $(window).trigger('hashchange');
-        expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
-    });
-});
+  });
+  
